1ffdd19c7a7a3e1ba9d9da1ac7951f6fa97ecb8f
azael <azael_jr04@icloud.com>
2024-02-16 22:18:12 -0600

    first commit

---
+++ lamina3/imprimir.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    int unNumero = 3;
+    int otroNumero = 4;
+
+    // quiero iprmir la suma de rres mas cuatro 
+    printf("%d",unNumero + otroNumero);
+    printf ("\n");
+    return 0;
+
+}
